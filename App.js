a711c9323c7e9aaa7d0653bd9113eddc2fa61fe8
Nana Ampomah Arhin <anarhin002.st.ug.edu.gh>
2024-05-27 11:18:10 -0700

    made the name Nana Ampomah Arhin bold

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ fontSize: 24 }}>My name is Nana Ampomah Arhin</Text>
+      <Text style={{ fontSize: 24 }}>
+        My name is
+        <Text style={{ fontWeight: "bold" }}>Nana Ampomah Arhin</Text>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
